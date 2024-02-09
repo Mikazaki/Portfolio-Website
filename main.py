e954698ad3ddf54c66649eca5dc88baf5498b72a
Mikazaki <messikazi2121@gmail.com>
2024-02-09 00:19:42 -0500

    First Commit

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap5
+import smtplib
+import os
+
+app = Flask(__name__)
+Bootstrap5(app)
+
+EMAIL_PASSWORD='uqwjawnamvtmqnuy'
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/portfolio')
+def portfolio():
+    return render_template('portfolio.html')
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+
+
+@app.route('/contact', methods=['POST', 'GET'])
+def contact():
+    if request.method == "POST":
+        user = request.form["name"]
+        email = request.form["email"]
+        phone = request.form["phone"]
+        message = request.form['message']
+        
+        sendemail(user, email, phone, message)
+        return render_template("contact.html", sent = True)
+    return render_template("contact.html", sent = False)
+
+def sendemail(name, email, phone, message):
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(user='pythontemp69@gmail.com', password=EMAIL_PASSWORD)
+            connection.sendmail(
+                from_addr='pythontemp69@gmail.com',
+                to_addrs='messikazi2121@gmail.com',
+                msg=f"Subject: New Message \n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}"
+            )
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
